3408022c46a72cb547967627174b1221bfd89720
maelg <mael.guillossou@gmail.com>
2013-12-10 00:08:29 +0100

    Notification, owner, and encoding

--- add.php
+++ add.php
@@ -13,6 +13,12 @@
 				width: 49%;
 				box-sizing: border-box;
 			}
+			.add div {
+				display: inline-block;
+				width: 49%;
+				box-sizing: border-box;
+				vertical-align: top;
+			}
 		</style>
 	</head>
 	<body>
@@ -34,14 +40,25 @@
 			</footer>
 		</div>
 		<div id="content">
-			<form class="bloc" method="post" action="add.php">
-				<h2>Recherche avec un code ISBN</h2>
-				<p>
+			<form class="bloc add" method="post" action="add.php">
+				<div>
+								<h2>Recherche avec un code ISBN</h2>
+
 					<input type="text" name="isbn" id="isbn" placeholder="Code ISBN"/>
+									<p>
+					<a href="#" class="btn">Rechercher</a>
 				</p>
-				<p>
-					<a href="#" class="btn">Connexion</a>
-				</p>
+
+				</div>
+<?php $owner = (!isset($_POST['owner'])) ? null : $_POST['owner']; ?>
+				<div>
+					<h3>Livre appartenant à</h3>
+					<input type="radio" name="owner" value="thierry" id="thierry" <?php if($owner == 'thierry') echo 'checked'; ?> /> <label for="thierry">Thierry</label><br>
+			        <input type="radio" name="owner" value="beatrice" id="beatrice" <?php if($owner == 'beatrice') echo 'checked'; ?> /> <label for="beatrice">Béatrice</label><br>
+			        <input type="radio" name="owner" value="mathilde" id="mathilde" <?php if($owner == 'mathilde') echo 'checked'; ?> /> <label for="mathilde">Mathilde</label><br>
+			        <input type="radio" name="owner" value="mael" id="mael" <?php if($owner == 'mael') echo 'checked'; ?> /> <label for="mael">Maël</label><br>
+			        <input type="radio" name="owner" value="elouan" id="elouan" <?php if($owner == 'elouan') echo 'checked'; ?> /> <label for="elouan">Elouan</label>
+			    </div>
 			</form>
 <?php
 if(isset($_POST['isbn']))
@@ -62,42 +79,74 @@ if(isset($_POST['isbn']))
 	    $infos['auteur'] = $book->volumeInfo->authors[0];  
 	    $infos['langue'] = $book->volumeInfo->language;  
 	    $infos['publication'] = $book->volumeInfo->publishedDate;  
-	    $infos['pages'] = $book->volumeInfo->pageCount;  
+	    $infos['pages'] = $book->volumeInfo->pageCount;
+	    $infos['proprietaire'] = $owner;
 	  
 	    if( isset($book->volumeInfo->imageLinks) )
 	        $infos['image'] = str_replace('&edge=curl', '', $book->volumeInfo->imageLinks->thumbnail);  
 	    else
 	    	$infos['image'] = null;
-	  
-	}  
-	else{  
-	   echo 'Livre introuvable';  
-	}
 ?>
 			<div class="bloc livre">
 				<div>
 					<img src="<?php echo $infos['image']; ?>" style="float: left;" alt="">
 					<p class="info"><?php echo $infos['titre']; ?> - <?php echo $infos['auteur']; ?><br><small><?php echo $infos['isbn']; ?></small><br>Nbr de pages <?php echo $infos['pages']; ?></p>
 				</div>
-				<div>
+				<div class="confirm">
 					<p>Est-ce le bon livre ?</p>
 					<p><a href="#" class="btn" id="confirm">Oui</a><a href="#" class="btn btn-danger">Non</a></p>
 				</div>
 			</div>
+<?php
+	}
+	else{  
+	   echo 'Livre introuvable';  
+	}
+?>
 			<script type="text/javascript">
+				function notifyMe(msg, tps) {
+				  // Let's check if the browser supports notifications
+				  if (!("Notification" in window)) {
+				    alert("This browser does not support desktop notification");
+				  }
+
+				  // Let's check if the user is okay to get some notification
+				  else if (Notification.permission === "granted") {
+				    // If it's okay let's create a notification
+				    var notification = new Notification(msg);
+				    setTimeout(function(){
+						notification.close();
+					}, tps);
+
+				  }
+
+				  // Otherwise, we need to ask the user for permission
+				  // Note, Chrome does not implement the permission static property
+				  // So we have to check for NOT 'denied' instead of 'default'
+				  else if (Notification.permission !== 'denied') {
+				    Notification.requestPermission(function (perm) {
+			        if (perm == 'granted') {
+			            notifyMe(msg, tps)
+			        }
+			    });
+				}
+			} 
+
 				if($('#confirm'))
 					$('#confirm').focus();
 				else
 					$('#isbn').focus();
 
 				$('#confirm').click(function() {
+					$('.confirm').slideUp();
 					$.ajax({
 					  type: "POST",
 					  url: "ajax/add.php",
 					  data: {book : <?php echo json_encode($infos); ?>}
 					})
 					  .done(function( msg ) {
-					    alert( "Data Saved: " + msg );
+					    notifyMe('Livre enregistré', 5000)
+					    $('#isbn').focus();
 					});
 				});
 			</script>
--- ajax/add.php
+++ ajax/add.php
@@ -6,14 +6,14 @@ $book = $_POST['book'];
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=mael', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=mael', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 }
 catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
 
-$req = $bdd->prepare('INSERT INTO logiblio_books VALUES("", :isbn, :titre, :auteur, :langue, :publication, :pages, :image)');
+$req = $bdd->prepare('INSERT INTO logiblio_books VALUES("", :isbn, :titre, :auteur, :langue, :publication, :pages, :image, :proprietaire)');
 $req->execute(array(
 	'isbn' => $book['isbn'], 
 	'titre' => $book['titre'],
@@ -21,5 +21,6 @@ $req->execute(array(
 	'langue' => $book['langue'], 
 	'publication' => $book['publication'], 
 	'pages' => $book['pages'], 
-	'image' => $book['image']
+	'image' => $book['image'], 
+	'proprietaire' => $book['proprietaire']
 	));
